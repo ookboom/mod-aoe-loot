9a906e3119f19bf7780e8db672984eb21a6e191d
adminkg <ookboom@outlook.com>
2024-10-03 11:51:01 +0800

    添加模组通知汉化

---
+++ data/sql/db-world/base/aoe_loot_acore_string_zhCN.sql
@@ -0,0 +1,2 @@
+UPDATE `acore_string` SET `locale_zhCN`='|cff4CFF00[Loot aoe]|r 服务器模组正在运行。' WHERE  `entry`=50000;
+UPDATE `acore_string` SET `locale_zhCN`='|cff4CFF00[Loot aoe]|r 你的物品将会通过邮件发送给你，请注意查收。' WHERE  `entry`=50001;
